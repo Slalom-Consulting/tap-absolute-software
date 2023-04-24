770377e3959a22f1ef21d79e81fea80a0de582c7
CJ Gonzalez <cj.gonzalez@slalom.com>
2023-04-24 16:55:25 -0400

    added core files

---
+++ tap_absolute_software/auth.py
@@ -0,0 +1,25 @@
+"""AbsoluteSoftware Authentication."""
+
+from __future__ import annotations
+
+from singer_sdk.authenticators import OAuthJWTAuthenticator
+
+
+class AbsoluteSoftwareAuthenticator(OAuthJWTAuthenticator):
+    """Authenticator class for AbsoluteSoftware."""
+
+    @classmethod
+    def create_for_stream(cls, stream) -> "AbsoluteSoftwareAuthenticator":
+        """Instantiate an authenticator for a specific Singer stream.
+
+        Args:
+            stream: The Singer stream instance.
+
+        Returns:
+            A new authenticator.
+        """
+        return cls(
+            stream=stream,
+            auth_endpoint="TODO: OAuth Endpoint URL",
+            oauth_scopes="TODO: OAuth Scopes",
+        )
---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+"""Test suite for tap-absolute-software."""
